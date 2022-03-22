ed652202f7989c5069b31a89194d959983ee842c
Ramonmurilo <ramonmurilo@id.uff.br>
2022-03-22 15:26:10 -0300

    ATUALIZAÇÃO: descritivo dentro do módulo

--- src/config.py
+++ src/config.py
@@ -1,3 +1,6 @@
+"""Arquivo de configurações do projeto
+"""
+
 caminho_base = 'C:/scripts/BANCO_METEOROLOGIA/'
 caminho_env = f'{caminho_base}/src'
 caminho_bancodedados = r'J:\SEDE\Comercializadora de Energia\6. MIDDLE\03.PLANILHAS\BASES_DASH_METEOROLOGIA'
