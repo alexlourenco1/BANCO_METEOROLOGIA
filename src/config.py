f2c292543eedb48e31d03a52702de5201ab7794a
Ramonmurilo <ramonmurilo@id.uff.br>
2022-03-25 14:41:25 -0300

    ADAPTAÇÃO: caminhos para ambiente de produção e de desenvolvimento

--- src/config.py
+++ src/config.py
@@ -1,7 +1,27 @@
 """Arquivo de configurações do projeto
 """
+######################################
+# CAMINHOS PARA AMBIENTE DE PRODUÇÃO #
+######################################
 
+#caminho_base = 'C:/scripts/BANCO_METEOROLOGIA/'
+#caminho_env = f'{caminho_base}/src'
+#caminho_bancodedados = r'J:\SEDE\Comercializadora de Energia\6. MIDDLE\03.PLANILHAS\BASES_DASH_METEOROLOGIA'
+#caminho_saida_excel = r'C:\Users\Middle\OneDrive - Eneva S.A\5.Meteorologia_dados'
+
+
+###################################
+# CAMINHOS PARA AMBIENTE DE TESTE #
+###################################
+"""INSTRUÇÕES:
+
+1. Faça uma cópia das database do ambiente de produção para as pastas "backup" e "ambiente_de_desenvolvimento"
+2. Comente o bloco dos caminhos de produção e descomente o bloco do ambiente de teste
+3. Após o período de testes, faça as cópias, respectivamente:
+    db produção -> backup
+    db desenvolvimento -> produção
+"""
 caminho_base = 'C:/scripts/BANCO_METEOROLOGIA/'
 caminho_env = f'{caminho_base}/src'
-caminho_bancodedados = r'J:\SEDE\Comercializadora de Energia\6. MIDDLE\03.PLANILHAS\BASES_DASH_METEOROLOGIA'
-caminho_saida_excel = r'C:\Users\Middle\OneDrive - Eneva S.A\5.Meteorologia_dados'
+caminho_bancodedados=r'J:\SEDE\Comercializadora de Energia\6. MIDDLE\03.PLANILHAS\BASES_DASH_METEOROLOGIA\ambiente_de_desenvolvimento'
+caminho_saida_excel = r'J:\SEDE\Comercializadora de Energia\6. MIDDLE\03.PLANILHAS\BASES_DASH_METEOROLOGIA\ambiente_de_desenvolvimento'
